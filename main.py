77d8a249074273f7b9edc04f552076155002655d
Titus Lim <tituslhy@gmail.com>
2024-12-09 19:37:26 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,83 @@
+def add(a: int | float, b: int | float) -> int | float:
+    """Adds two values.
+
+    Args:
+        a (int | float) 
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a + b
+
+def minus(a: int | float, b: int | float) -> int | float:
+    """Minus operator. Deducts b from a
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a - b
+
+def divide(a: int | float, b: int | float) -> int | float:
+    """Division operator. Divides a by b
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a / b
+
+def multiply(a: int | float, b: int | float) -> int | float:
+    """Multiplier operator. Multiplies a by b
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a * b
+
+def modulo(a: int | float, b: int | float) -> int | float:
+    """Modulo operator. Returns the remainder of a/b
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a % b
+
+def integer_divide(a: int | float, b: int | float) -> int | float:
+    """Returns the integer after dividing a by b
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a // b
+
+def exponent(a: int | float, b: int | float) -> int | float:
+    """Exponent operator. Returns a raised to the power of b
+
+    Args:
+        a (int | float)
+        b (int | float)
+
+    Returns:
+        int | float
+    """
+    return a ** b
